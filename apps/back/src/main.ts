3a765fd011e703f72984818b44437f1949c483d1
babyyoda62406 <babyyoda62406@gmail.com>
2024-07-25 00:37:39 -0400

    config: Environment Variables

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envVars } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('app.module'); 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api/v1';
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(envVars.PORT);
+  logger.log(`Server is running on port ${envVars.PORT}`);
 }
 bootstrap();
